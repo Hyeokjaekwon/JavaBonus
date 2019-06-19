56dd7db715559c94bb17615c4672547445af77e5
Daniel <48001033+Hyeokjaekwon@users.noreply.github.com>
2019-06-20 00:02:12 +0900

    Add files via upload

---
+++ Hjls.java
@@ -0,0 +1,10 @@
+package edu.hyeokjaekwon.java.ls;
+
+public class Hjls {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Main.java
@@ -0,0 +1,9 @@
+package edu.hyeokjaekwon.java;
+
+import edu.hyeokjaekwon.java.ls.Hjls;
+
+public class Main {
+	public static void main(String[] args) {
+		new edu.hyeokjaekwon.java.Hjls().run(args);
+	}
+}
